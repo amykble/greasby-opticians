1a84990d19ad77e920beb5a369e19d9273507acf
amykble <56142077+amykble@users.noreply.github.com>
2020-06-09 16:17:03 +0100

    Tidying updates

--- dist/assets/js/page--src-pages-about-vue.20ef1517.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{163:function(t,e,n){"use strict";n.r(e);var u={metaInfo:{title:"About"}},o=n(24),s=Object(o.a)(u,(function(){var t=this.$createElement,e=this._self._c||t;return e("Layout",[e("h1",[this._v("About us")])])}),[],!1,null,null,null);e.default=s.exports}}]);
--- dist/assets/js/page--src-pages-index-vue.c9f2bc96.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{164:function(t,e,n){"use strict";n.r(e);var l={metaInfo:{title:"Home"}},o=n(24),u=Object(o.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("Layout")}),[],!1,null,null,null);e.default=u.exports}}]);
