c57be217a311fefc4d5ceebbf284a00202754eba
amykble <56142077+amykble@users.noreply.github.com>
2020-06-16 21:18:30 +0100

    Updated nav animation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,22 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.6.2] | 2020-06-16
+
+#### Added
+
+- Added navigation large area for close
+
+#### Changed
+
+- Chnaged navigation animation
+
+#### Fixed
+
+- x
+
+---
+
 ## [0.6.1] | 2020-06-16
 
 #### Added
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,11 +2,11 @@
   <header class="font-body">
     <section
       :class="isOpen ? 'appear' : 'disappear'"
-      class="nav-container fixed z-10 h-full w-64 bg-white shadow-md transition duration-300 ease-in-out"
+      class="nav-container fixed z-20 h-full w-64 bg-white shadow-md transition duration-300 delay-100 ease-in-out"
     >
       <nav
         :class="isOpen ? 'opacity' : 'none'"
-        class="transition duration-300 delay-300 ease-in-out"
+        class="transition duration-300 delay-400 ease-in-out"
       >
         <a href="#" class="focus:outline-none">
           <g-image
@@ -35,54 +35,57 @@
         <hr class="w-56 ml-4" />
         <hr class="w-56 ml-4 mt-12" />
         <g-link
-          to="//"
+          to="/"
           class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
         >NHS</g-link>
         <g-link
-          to="//"
+          to="/"
           class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
         >Kids</g-link>
         <g-link
-          to="//"
+          to="/"
           class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
         >Frames</g-link>
         <g-link
-          to="//"
+          to="/"
           class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
         >Contact Lenses</g-link>
         <hr class="w-56 ml-4" />
         <div class="absolute bottom-0">
           <hr class="w-56 ml-4" />
           <g-link
-            to="//"
+            to="/"
             class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
           >FAQ</g-link>
           <g-link
-            to="//"
+            to="/"
             class="block my-2 mx-4 hover:text-gray-500 focus:text-gray-500 transition duration-200 ease-in-out"
           >Contact Us</g-link>
           <hr class="w-56 ml-4 mb-4" />
         </div>
       </nav>
     </section>
-    <section class="fixed z-10">
+    <section class="fixed z-20">
       <button
         @click="isOpen = !isOpen"
         :class="isOpen ? 'ml-66' : 'ml-2'"
         type="button"
-        class="hamburger m-2 p-2 w-14 border-transparent bg-transparent transition-all duration-300 ease-in-out"
+        class="hamburger m-2 p-2 w-14 border-transparent bg-transparent transition-all duration-300 delay-100 ease-in-out"
       >
+        <span class="slice slice-1 w-8 h-1 block bg-white transition-all duration-200 ease-in-out"></span>
         <span
-          class="slice slice-1 w-8 h-1 block bg-greasby transition-all duration-200 ease-in-out"
+          class="slice slice-2 w-10 h-1 mt-1 block bg-white transition-all duration-200 ease-in-out"
         ></span>
         <span
-          class="slice slice-2 w-10 h-1 mt-1 block bg-greasby transition-all duration-200 ease-in-out"
-        ></span>
-        <span
-          class="slice slice-3 w-6 h-1 mt-1 block bg-greasby transition-all duration-200 ease-in-out"
+          class="slice slice-3 w-6 h-1 mt-1 block bg-white transition-all duration-200 ease-in-out"
         ></span>
       </button>
     </section>
+    <section
+      @click="isOpen = !isOpen"
+      :class="isOpen ? 'show' : 'hide'"
+      class="fixed z-10 w-full h-screen bg-black opacity-70 transition-all duration-200 delay-100 ease-in-out"
+    ></section>
   </header>
 </template>
 
@@ -92,23 +95,8 @@ export default {
 	name: 'Navigation',
 	data() {
 		return {
-			isOpen: Boolean,
+			isOpen: false,
 		}
-	},
-	methods: {
-		getViewWidth() {
-			if (process.isClient && window.innerWidth < 1280) {
-				this.isOpen = false
-			} else if (process.isClient && window.innerWidth >= 1281) {
-				this.isOpen = true
-			} else {
-				console.log('window undefined')
-				this.isOpen = true
-			}
-		},
-	},
-	mounted() {
-		this.getViewWidth()
 	},
 }
 </script>
@@ -159,5 +147,15 @@ button:hover .slice-3 {
 
 .none {
 	opacity: 0;
+}
+
+.show {
+	opacity: 0.8;
+	visibility: visible;
+}
+
+.hide {
+	opacity: 0;
+	visibility: hidden;
 }
 </style>
