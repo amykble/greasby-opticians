dd7ccdda2ea73dbb780c16a68ca5130d60fcb84c
amykble <56142077+amykble@users.noreply.github.com>
2020-06-23 23:35:39 +0100

    Added coming soon

--- src/pages/Technology.vue
+++ src/pages/Technology.vue
@@ -1,5 +1,11 @@
 <template>
-  <Layout>New Technology</Layout>
+  <Layout>
+    <section class="bg-gray-900 text-white h-screen font-body">
+      <div class="max-w-screen-lg mx-auto h-screen flex items-center justify-center">
+        <h1 class="mx-6 text-2xl text-center">Page Coming Soon</h1>
+      </div>
+    </section>
+  </Layout>
 </template>
 
 <script>
