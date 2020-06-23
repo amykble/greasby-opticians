7e5292804c0443ba21b471f9aaa1518fb3b91e67
amykble <56142077+amykble@users.noreply.github.com>
2020-06-23 22:22:29 +0100

    Tidied frames page

--- src/pages/frames.vue
+++ src/pages/frames.vue
@@ -11,6 +11,112 @@
         class="text-xl text-center pt-10 mx-6"
       >We have worked hard to build relationships so that we can stock the frame types, styles and designer names that you want to wear, there's something for everyone.</h2>
     </section>
+    <section class="messages relative w-64 mx-auto">
+      <div
+        :class="messageOne ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-24 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>ANNA SUI’S collections take you on a creative journey that is unparralleled in the world of fashion. Mixing vintage styles with her current cultural obsessions, she effortlessly makes hip, exuberant, original designs</p>
+        </div>
+      </div>
+      <div
+        :class="messageTwo ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-24 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>The BOSS Orange collection is the perfect combination of style and personality. Contemporary frame shapes, colour and detail sit alongside classic designs to offer a choice of styles from high fashion to urban cool.</p>
+        </div>
+      </div>
+      <div
+        :class="messageThree ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-24 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>The new and exciting additional range of Men's and women's eyewear follows the trends of the Cafe Biker scene of the 50s and 60s, highlighting strong masculine shapes in dark tones and contrast ladies' models in small cats' eyes.</p>
+        </div>
+      </div>
+      <div
+        :class="messageFour ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-40 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Emporio Armani glasses are widely recognisable with its iconic eagle logo. They have a cult celebrity following behind them and it is obvious why they are so popular they display absolute class and sophistication, combining the right mix of comfort and durability and style.</p>
+        </div>
+      </div>
+      <div
+        :class="messageFive ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-60 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>For more than 80 years, Revlon has brought the most beautiful colors to the most beautiful women in the world. Revlon Eyewear encourages women to express their fabulous femininity.</p>
+        </div>
+      </div>
+      <div
+        :class="messageSix ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-60 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Radley started eyewear in Spring Summer 2011. “The collection is designed to appeal to discerning, confident, and fun loving women who are fashionable but have an independent sense of style.”</p>
+        </div>
+      </div>
+      <div
+        :class="messageSeven ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-60 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Jaeger is a long-established, instantly recognisable brand with an excellent reputation. As well as a classic British style, the frames provide a diverse range of styles to fit a variety of needs. A truly iconic collection.</p>
+        </div>
+      </div>
+      <div
+        :class="messageEight ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-78 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Best known for its iconic styles in designer shades, Ray Ban also offers prescription eyeglasses in the same highly distinctive and timeless RX frame styles, such as the consistently popular wayfarers, aviators, and Clubmaster frames the brand originally created—plus, of course, many other stylish and contemporary premium spectacles to suit any face shape or style preference.</p>
+        </div>
+      </div>
+      <div
+        :class="messageNine ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-78 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Prada enjoys a reputation as one of the world's premiere fashion brands. Their designs are tasteful and artistic - and always fashion-forward. Add a touch of Prada sophistication to your everyday look with a new pair of glasses.</p>
+        </div>
+      </div>
+      <div
+        :class="messageTen ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-78 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Combining Japanese street wear culture & vintage Americana style, this Superdry range is not to be missed. Taking inspiration from classic styles and modern finishes, the Superdry eyewear collection is perfect for those seeking timeless elegance.</p>
+        </div>
+      </div>
+      <div
+        :class="messageEleven ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-24 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>The Ferucci collection is the ultimate blend of elegant designs and luxury materials. The traditional shapes carry depth for multifocals and the intricate side detailing creates a collection that’s as classical as ever.</p>
+        </div>
+      </div>
+      <div
+        :class="messageTwelve ? 'show' : 'hide'"
+        class="absolute left-0 top-0 -mt-6 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>Charmant eyeware strives to provide state-of-the-art eyeware in both conservative and modern designs. This makes Charmant the ideal collection for anyone looking to find the best in value and style. The Charmant Titanium Collection blends the durability and beauty of titanium with modern designs to create a collection that is truly built to last.</p>
+        </div>
+      </div>
+      <div
+        :class="messageLast ? 'show' : 'hide'"
+        class="absolute left-0 top-0 mt-24 z-10 flex justify-center items-center transition duration-300 ease-in-out"
+      >
+        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
+          <p>The Bulgari eyewear collection reflects women’s innate beauty, enhancing the different facets of femininity: dazzling exuberance, stylish urban spirit or elegant charm, all with a distinctive appeal. The Le Gemme precious collection is jewellery for the eyes, featuring noble materials & timeless designs.</p>
+        </div>
+      </div>
+    </section>
     <section class="max-w-screen-lg mx-auto py-10 px-6 w-full font-body">
       <div class="flex items-center justify-center mb-3 sm:mb-8">
         <div @mouseover="messageOne = true" @mouseleave="messageOne = false" class="cursor-pointer">
@@ -150,112 +256,6 @@
         <p class="brand-container-7 opacity-0 mx-6">and more brands and styles to choose from...</p>
       </div>
     </section>
-    <section class="messages relative w-64 mx-auto">
-      <div
-        :class="messageOne ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>ANNA SUI’S collections take you on a creative journey that is unparralleled in the world of fashion. Mixing vintage styles with her current cultural obsessions, she effortlessly makes hip, exuberant, original designs</p>
-        </div>
-      </div>
-      <div
-        :class="messageTwo ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>The BOSS Orange collection is the perfect combination of style and personality. Contemporary frame shapes, colour and detail sit alongside classic designs to offer a choice of styles from high fashion to urban cool.</p>
-        </div>
-      </div>
-      <div
-        :class="messageThree ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>The new and exciting additional range of Men's and women's eyewear follows the trends of the Cafe Biker scene of the 50s and 60s, highlighting strong masculine shapes in dark tones and contrast ladies' models in small cats' eyes.</p>
-        </div>
-      </div>
-      <div
-        :class="messageFour ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Emporio Armani glasses are widely recognisable with its iconic eagle logo. They have a cult celebrity following behind them and it is obvious why they are so popular they display absolute class and sophistication, combining the right mix of comfort and durability and style.</p>
-        </div>
-      </div>
-      <div
-        :class="messageFive ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>For more than 80 years, Revlon has brought the most beautiful colors to the most beautiful women in the world. Revlon Eyewear encourages women to express their fabulous femininity.</p>
-        </div>
-      </div>
-      <div
-        :class="messageSix ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Radley started eyewear in Spring Summer 2011. “The collection is designed to appeal to discerning, confident, and fun loving women who are fashionable but have an independent sense of style.”</p>
-        </div>
-      </div>
-      <div
-        :class="messageSeven ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Jaeger is a long-established, instantly recognisable brand with an excellent reputation. As well as a classic British style, the frames provide a diverse range of styles to fit a variety of needs. A truly iconic collection.</p>
-        </div>
-      </div>
-      <div
-        :class="messageEight ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Best known for its iconic styles in designer shades, Ray Ban also offers prescription eyeglasses in the same highly distinctive and timeless RX frame styles, such as the consistently popular wayfarers, aviators, and Clubmaster frames the brand originally created—plus, of course, many other stylish and contemporary premium spectacles to suit any face shape or style preference.</p>
-        </div>
-      </div>
-      <div
-        :class="messageNine ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Prada enjoys a reputation as one of the world's premiere fashion brands. Their designs are tasteful and artistic - and always fashion-forward. Add a touch of Prada sophistication to your everyday look with a new pair of glasses.</p>
-        </div>
-      </div>
-      <div
-        :class="messageTen ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Combining Japanese street wear culture & vintage Americana style, this Superdry range is not to be missed. Taking inspiration from classic styles and modern finishes, the Superdry eyewear collection is perfect for those seeking timeless elegance.</p>
-        </div>
-      </div>
-      <div
-        :class="messageEleven ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>The Ferucci collection is the ultimate blend of elegant designs and luxury materials. The traditional shapes carry depth for multifocals and the intricate side detailing creates a collection that’s as classical as ever.</p>
-        </div>
-      </div>
-      <div
-        :class="messageTwelve ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>Charmant eyeware strives to provide state-of-the-art eyeware in both conservative and modern designs. This makes Charmant the ideal collection for anyone looking to find the best in value and style. The Charmant Titanium Collection blends the durability and beauty of titanium with modern designs to create a collection that is truly built to last.</p>
-        </div>
-      </div>
-      <div
-        :class="messageLast ? 'show' : 'hide'"
-        class="absolute left-0 top-0 z-10 flex justify-center items-center transition duration-300 ease-in-out"
-      >
-        <div class="w-64 p-4 bg-gray-900 text-white rounded-md shadow-md">
-          <p>The Bulgari eyewear collection reflects women’s innate beauty, enhancing the different facets of femininity: dazzling exuberance, stylish urban spirit or elegant charm, all with a distinctive appeal. The Le Gemme precious collection is jewellery for the eyes, featuring noble materials & timeless designs.</p>
-        </div>
-      </div>
-    </section>
     <section class="bg-greasby text-white py-10">
       <div class="max-w-screen-lg mx-auto">
         <h2 class="text-2xl text-center mx-6 mb-4">Choosing Your Glasses</h2>
@@ -353,17 +353,33 @@ button:hover {
 	@apply outline-none bg-opacity-30 shadow-lg;
 }
 
+@keyframes show {
+	from {
+		visibility: hidden;
+		opacity: 0;
+	}
+	to {
+		visibility: visible;
+		opacity: 1;
+	}
+}
+
+@keyframes hide {
+	from {
+		visibility: visible;
+		opacity: 1;
+	}
+	to {
+		visibility: hidden;
+		opacity: 0;
+	}
+}
+
 .show {
-	visibility: visible;
-	opacity: 1;
-	transform: scaleY(1);
-	transform-origin: top;
+	animation: show 300ms forwards;
 }
 
 .hide {
-	visibility: 0;
-	opacity: 0;
-	transform: scaleY(0);
-	transform-origin: top;
+	animation: hide 300ms forwards;
 }
 </style>
